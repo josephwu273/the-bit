b49be322c575811a1419d481224754371c6a2898
josephwu273 <josephwu@buffalo.edu>
2023-09-05 00:43:39 -0400

    Committing to the bit

--- commit-to-the-bit.py
+++ commit-to-the-bit.py
@@ -2,8 +2,10 @@ from os.path import getmtime
 from pathlib import Path
 from subprocess import run
 from datetime import datetime
+import re
 
-FILE_PATH = Path.cwd()/"bit.log"
+FILE_PATH = Path.cwd()/"README.md"
+TITLE = "# the-bit\n"
 TODAY = datetime.today().date()
 MESSAGE = f"On {TODAY}, I committed to the bit!"
 
@@ -23,15 +25,20 @@ def date_modified(file):
     time_modified = getmtime(file)
     return datetime.fromtimestamp(time_modified).date()
 
-
-if  __name__ == "__main__":
+def main():
     if FILE_PATH.is_file():
         if date_modified(FILE_PATH) < TODAY:
             with open(FILE_PATH, "r") as f:
                 file_contents = f.read()
-            contents = MESSAGE + "\n" + file_contents
+            reg = re.compile(r"(?P<title>#.*?\n)(?P<log>.*)", re.S)
+            parsed_file = reg.match(file_contents)
+            contents = TITLE + MESSAGE + "\n\n" + parsed_file.group("log")
         else:
             contents = ""
     else:
-        contents = MESSAGE
+        contents = TITLE + MESSAGE
     update_log(contents)
+
+
+if  __name__ == "__main__":
+    main()
