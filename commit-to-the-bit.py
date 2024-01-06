3bb9f704dbb0fd177e243c63d6860ad167225a2c
josephwu273 <josephwu@buffalo.edu>
2024-01-05 23:29:59 -0500

    Commits now based on README not date_modified

--- commit-to-the-bit.py
+++ commit-to-the-bit.py
@@ -1,13 +1,14 @@
-from os.path import getmtime
 from pathlib import Path
 from subprocess import run
 from datetime import datetime
 import re
+from dateutil import parser
 
 FILE_PATH = Path.cwd()/"README.md"
 TITLE = "# the-bit\n"
 TODAY = datetime.today().date()
 MESSAGE = f"On {TODAY}, I committed to the bit!"
+REG = re.compile(r"(?P<title>#.*?\n\n)(?P<latest>.*?\n)(?P<log>.*)", re.S)
 
 def update_log(contents):
     if not contents:
@@ -21,24 +22,22 @@ def update_log(contents):
     except Exception as e:
         print(f"An error occurred: {e}")
 
-def date_modified(file):
-    time_modified = getmtime(file)
-    return datetime.fromtimestamp(time_modified).date()
-
 def main():
     if FILE_PATH.is_file():
-        if date_modified(FILE_PATH) < TODAY:
-            with open(FILE_PATH, "r") as f:
-                file_contents = f.read()
-            reg = re.compile(r"(?P<title>#.*?\n)(?P<log>.*)", re.S)
-            parsed_file = reg.match(file_contents)
-            contents = TITLE + MESSAGE + "\n\n" + parsed_file.group("log")
+        with open(FILE_PATH) as f:
+            file_contents = f.read()
+        parsed_file = REG.match(file_contents)
+        if parsed_file:
+            latest = parsed_file.group("latest")
+            latest_date = parser.parse(latest, fuzzy=True).date()
+            if latest_date<TODAY:
+                contents = TITLE + "\n" + MESSAGE + "\n" + parsed_file.group("log")
+            else:
+                contents = ""
         else:
-            contents = ""
-    else:
-        contents = TITLE + MESSAGE
+            contents = TITLE + MESSAGE
     update_log(contents)
 
-
 if  __name__ == "__main__":
+    print("hello")
     main()
